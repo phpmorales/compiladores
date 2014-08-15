c6cd9ada8c878358e6957af4ce75c1fe456215ff
phpmorales <phpmorales@inf.ufrgs.br>
2014-08-15 08:23:48 -0700

    Altos testes... parece estar ficando ok!

--- etapa1/main.c
+++ etapa1/main.c
@@ -6,23 +6,44 @@ int main(int argc, char *argv[])
 	
 	if (argc<2)
 	{
-		fprintf(stderr, "call with the file name...\n");
+		fprintf(stderr, "ops! faltou chamar o nome do arquivo. Exemplo: etapa1 <nome_do_arquivo>\n");
 		exit (1);
 	}
 	if (!(yyin = fopen(argv[1], "r")))
 	{
-		fprintf(stderr, "Cannot open file\n");
+		fprintf(stderr, "Houve algum erro. Não é possível abrir o arquivo!\n");
+		fprintf(stderr, "Verifique se o nome do arquivo está correto e tente novamente!\n");
 		exit(1);
 	}
+
+	system("clear");
+	printf("\n-----------------------------------------");
+	printf("\nUFRGS - INSITITUTO DE INFORMATICA");
+	printf("\nCOMPILADORES 2014/2");
+	printf("\nMiller Biazus");
+	printf("\nPedro Morales");
+	printf("\n-----------------------------------------\n");
+
+	printf("\n-----------------------------------------");
+	printf("\nAGORA SERÁ REALIZADA A LEITURA DO ARQUIVO");
+	printf("\nAGUARDE, REALIZANDO PARSER...");
+	printf("\n-----------------------------------------\n");
 	
 	while (running)
 	{
 		simb = yylex();
 		if(simb)
-			printf("linha %d token %d \n", LineNumber, simb);
+			printf("Linha: %d - Token: %d \n", LineNumber, simb);
 	}
-	
+	printf("-----------------------------------------");
+	printf("\nARQUIVO LIDO COM SUCESSO:");
+	printf("\n-----------------------------------------\n");
 
    	hashPrint(&hashTable);
+
+        printf("\n-----------------------------------------");
+	printf("\nETAPA 1 FINALIZADA!");
+	printf("\n-----------------------------------------\n\n");
+
 	return 0;
 }
--- etapa1/tokens.h
+++ etapa1/tokens.h
@@ -6,34 +6,33 @@
  * Assim, nao conflitam entre si e com os tokens representados pelo proprio
  * valor ascii de caracteres isolados. 
  */
+ 
+#define KW_WORD       256
+#define KW_BOOL       258
+#define KW_BYTE       259
+#define KW_IF         261
+#define KW_THEN       262
+#define KW_ELSE       263
+#define KW_LOOP       264
+#define KW_INPUT      266
+#define KW_RETURN     267
+#define KW_OUTPUT     268
 
-#define KW_WORD 256
-#define KW_BOOL 258
-#define KW_BYTE 259
-#define KW_IF 261
-#define KW_THEN 262
-#define KW_ELSE 263
-#define KW_LOOP 264
-#define KW_INPUT 266
-#define KW_RETURN 267
-#define KW_OUTPUT 268
-
-#define OPERATOR_LE 270
-#define OPERATOR_GE 271
-#define OPERATOR_EQ 272
-#define OPERATOR_NE 273
-#define OPERATOR_AND 274
-#define OPERATOR_OR 275
+#define OPERATOR_LE   270
+#define OPERATOR_GE   271
+#define OPERATOR_EQ   272
+#define OPERATOR_NE   273
+#define OPERATOR_AND  274
+#define OPERATOR_OR   275
 
 #define TK_IDENTIFIER 280
+#define LIT_INTEGER   281
+#define LIT_FALSE     283
+#define LIT_TRUE	284
+#define LIT_CHAR      285
+#define LIT_STRING    286
 
-#define LIT_INTEGER 281
-#define LIT_FALSE 283
-#define LIT_TRUE 284
-#define LIT_CHAR 285
-#define LIT_STRING 286
-
-#define TOKEN_ERROR 290
+#define TOKEN_ERROR   290
 
 /* END OF FILE */
 
